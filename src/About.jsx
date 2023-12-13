d77d1e86c79b3faab1c2153cec22eb0a5fd30a54
Caleb Hemphill <calebhemp@gmail.com>
2023-12-12 19:02:56 -0500

    Update about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,16 @@
-import { Component } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
 
-class Profile extends Component {
-
-  render() {
-    /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
-  }
+function Profile() {
+  return (
+    <ListGroup>
+      <ListGroup.Item variant='primary'>Developed By:</ListGroup.Item>
+      <ListGroup.Item action href='https://github.com/Ugo-Obiako' target='_blank'>Ugo Obiako</ListGroup.Item>
+      <ListGroup.Item action href='https://github.com/kaylubh' target='_blank'>Caleb Hemphill</ListGroup.Item>
+      <ListGroup.Item variant='primary'>GitHub Repos:</ListGroup.Item>
+      <ListGroup.Item action href='https://github.com/Ugo-Obiako/can-of-books-frontend' target='_blank'>App Client</ListGroup.Item>
+      <ListGroup.Item action href='https://github.com/kaylubh/can-of-books-backend' target='_blank'>App Server</ListGroup.Item>
+    </ListGroup>
+  )
 }
 
 export default Profile;
