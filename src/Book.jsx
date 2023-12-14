2234dd4c7088e09cee898267e4f333192645bf74
Ugo Obiako <obiakou@yahoo.com>
2023-12-13 18:01:18 -0800

    Updated file

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,20 +1,31 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 
 
 
 export default function Book(props) {
-    return (
 
+    function handleDelete() {
+        props.onDelete(props.id);
+        console.log(props.id)
+    }
+
+    return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="https://placehold.co/600x400" />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>
                     {props.description}
+                    <Button onClick={handleDelete}> Delete book</Button>
                 </Card.Text>
             </Card.Body>
+            <Card.Footer>
+
+
+            </Card.Footer>
         </Card>
 
     )
